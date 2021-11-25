5b4f47459a2dfa6db83c046398e440f4b1ea1c19
Ivan Pavlov <pavlov200912@gmail.com>
2021-11-25 11:46:13 +0300

    clear unchosen subtrees

--- ui.js
+++ ui.js
@@ -9,13 +9,14 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
                 if (typeof solution.output_values[key] == "boolean") {
                     isChosen = isChosen | solution.output_values[key]
                 } else {
-                    isChosen = isChosen | (solution.output_values[key] == 'yes')
+                    isChosen = isChosen | (solution.output_values[key] === 'yes')
                 }
             }
         }
 
         const sub_tree_lists = [[0, 1, 2, 3], [4, 5, 6], [7, 8, 9, 10], [11, 12, 13], [14, 15, 16, 17]]
         var choosen_sub_trees = [false, false, false, false, false]
+        var chosen_clusters = []
 
         for (let key in solution.output_values) {
             if (key.includes('cluster')) {
@@ -23,10 +24,11 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
                 if (typeof solution.output_values[key] == "boolean") {
                     is_true = solution.output_values[key]
                 } else {
-                    is_true = (solution.output_values[key] == 'yes')
+                    is_true = (solution.output_values[key] === 'yes')
                 }
                 const cluster_num = parseInt(key.split('_')[1])
                 if (is_true) {
+                    chosen_clusters.push(cluster_num)
                     for (let i = 0; i < 5; i++) {
                         if(sub_tree_lists[i].includes(cluster_num)) {
                             choosen_sub_trees[i] = true
@@ -42,6 +44,27 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
             if (choosen_sub_trees[i]) {
                 count += 1
             }
+        }
+
+
+        var other_empty = false
+
+        other_empty = chosen_clusters.includes(3)
+            && solution.output_values['ccluster_3_input'] == null
+        other_empty |= chosen_clusters.includes(10)
+            && solution.output_values['ccluster_10_input'] == null
+        other_empty |= chosen_clusters.includes(17)
+            && solution.output_values['ccluster_17_input'] == null
+
+        if (other_empty) {
+            return {
+                task_id: this.getTask().id,
+                errors: {
+                    '__TASK__': {
+                        message: "You chose other cluster but didn't fill the input box. Please clarify what do you mean by other."
+                    }
+                }
+            };
         }
 
         if (count > 1) {
@@ -94,7 +117,7 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
         true_d.style.display = solution.output_values.croot === 'yes' ? 'block' : 'none';
 
         false_d = root.querySelector('.false');
-        false_d.style.display = solution.output_values.croot == 'no' ? 'block': 'none';
+        false_d.style.display = solution.output_values.croot === 'no' ? 'block': 'none';
 
         {
             // 1
@@ -144,6 +167,46 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
             var false_d = root.querySelector('.false0');
             false_d.style.display = solution.output_values.croot0 === 'no' ? 'block' : 'none';
         }
+
+
+        const sub_tree_lists = [[0, 1, 2, 3], [4, 5, 6], [7, 8, 9, 10], [11, 12, 13], [14, 15, 16, 17]]
+
+        function clear_clusters(tree_index) {
+            for (let i = 0; i < 5; i++) {
+                if (i === tree_index) {
+                    continue
+                }
+                for (let v of sub_tree_lists[i]) {
+                    var cluster_name = 'ccluster_' + v.toString()
+                    solution.output_values[cluster_name] = false
+                    console.log("Clear tree ", i, cluster_name)
+                }
+            }
+        }
+
+        if (solution.output_values.croot === 'yes') {
+            if (solution.output_values.croot1 === 'yes') {
+                // tree 0
+                clear_clusters(0)
+            } else {
+                if (solution.output_values.croot10 === 'yes') {
+                    // tree 1
+                    clear_clusters(1)
+                } else {
+                    // tree 2
+                    clear_clusters(2)
+                }
+            }
+        } else {
+            if (solution.output_values.croot0 === 'yes') {
+                // tree 3
+                clear_clusters(3)
+            } else {
+                // tree 4
+                clear_clusters(4)
+            }
+        }
+
 
         console.log(solution.output_values);
         TolokaHandlebarsTask.prototype.setSolution.call(this, solution);
